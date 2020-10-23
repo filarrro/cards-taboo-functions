bc0a22b745e4decc7c03fcc9b79cea5eaf8b1a11
Mateusz Filar <mateusz.filar@miquido.com>
2020-10-23 16:07:05 +0200

    feat(Auth): Authorize user

---
+++ functions/authorize.js
@@ -0,0 +1,27 @@
+import querystring from "querystring";
+
+const USERS = {
+  'mateo!123': { id: 'mateo!123', name: 'Mateo' },
+  'asiulek!123': { id: 'asiulek!123', name: 'Asiulek' },
+  'gosiak!123': { id: 'gosiak!123', name: 'Gosiak' },
+  'raffi!123': { id: 'raffi!123', name: 'Raffi' },
+  'robus!123': { id: 'robercik!123', name: 'Robuś' },
+  'wiesiula!123': { id: 'wiesiula!123', name: 'Wiesiula' },
+}
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  // When the method is POST, the name will no longer be in the event’s
+  // queryStringParameters – it’ll be in the event body encoded as a query string
+  const params = querystring.parse(event.body);
+  const user = USERS[params.name];
+
+  return {
+    statusCode: !user ? 401 : 200,
+    body: user
+  };
+};
