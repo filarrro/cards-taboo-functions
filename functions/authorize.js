2e9113d88d9463072931e02396860f4f62f66986
Mateusz Filar <mateusz.filar@miquido.com>
2020-10-23 16:22:26 +0200

    fix(Auth): Remove export keyword

--- functions/authorize.js
+++ functions/authorize.js
@@ -1,6 +1,6 @@
 const querystring = require("querystring");
 
-export const headers = {
+const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
